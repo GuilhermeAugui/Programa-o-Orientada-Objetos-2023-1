22a41cd40c5fa03551573c43ba9c7ace0720ae9b
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-03-21 21:02:31 -0300

    commit

--- atividade2questao3.py
+++ atividade2questao3.py
@@ -8,9 +8,11 @@ class Data:
         print(self.dia,'/',self.mes,'/',self.ano)
 
     def impimeDataPorExtenssso(self, cidade):
+        # ano= ['Mil','Dois Mil','Tres Mil']
+        # ano1=['Cem','Duzentos','Trezentos','Quatrossentos','Quinhentos', 'Seissentos', 'Setesentos', 'Oiticentos', 'Novecentos']
         messes= ['janeiro','fevereiro','março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro' ,'novembro' ,'dezembro']
         messes_por_extensso= messes[self.mes-1]
-        print(self.dia,'/',self.mes,'/',self.ano)
+        print( cidade ,self.dia,',',messes_por_extensso,',',self.ano)
 
 d= Data(20,2,2004)
 d.imprimeData()
